ac3ccd0b058893408b41caa84c6baddca6e1e8ea
Deepak gupta <sg7052551819@gmail.com>
2026-08-03 19:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (11.36%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -18,16 +18,16 @@ bool checksubtree(TreeNode* root,TreeNode* subRoot){
 }
 
 bool issubtree(TreeNode* root, TreeNode* subRoot){
+    // these condn are also  currect but it is gieven than  root and sub root has at least one node  so these cond are not needed however you can also use this condn but here instead of these three we used condn line 25 for base case when we travrse thrgough all node and dosent fonsd root == subroot
 // if(root==NULL && subRoot ==NULL   ) return true;
 // if(root!=NULL && subRoot ==NULL)    return true;
 // if(root==NULL && subRoot !=NULL)  return false;
 if(root==NULL) return false;
 if(root->val == subRoot->val){
    if (checksubtree(root,subRoot)) return true;
+   //here we ddint check for false beacuse if we return false then remaning tree will not procced and what if we have our ans somewhereelse
 }
  return issubtree(root->left,subRoot) || issubtree(root->right,subRoot);
-
-
     }
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
