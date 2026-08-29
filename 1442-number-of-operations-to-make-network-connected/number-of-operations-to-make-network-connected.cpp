ad88e8cc6e7a7f15b57f59b50f10ae20d7785c96
Deepak gupta <sg7052551819@gmail.com>
2026-08-30 00:41:25 +0530

    Time: 20 ms (46.41%) | Memory: 46.6 MB (56.60%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,65 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int Find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = Find(parent[x]);
+    }
+    void Union(int x,int y){
+        int x_parent=Find(x);
+        int y_parent=Find(y);
+
+        if(x_parent==y_parent){
+            return;
+        }
+
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }
+        else if(rank[y_parent]>rank[x_parent]){
+            parent[x_parent]=y_parent;
+        }
+        else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++; 
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+    DSU dsu(n);
+        int redundant=0;
+        int req=n-1;
+        int count=0;
+    for(auto edge:connections){
+        int u=edge[0];
+        int v=edge[1];
+     
+        if(dsu.Find(u)!=dsu.Find(v)){
+            dsu.Union(u,v);
+            count++;
+        }
+        else{
+        redundant++;
+        }
+    }
+  if(redundant>=req-count){
+    return req-count;
+  }
+  return -1;
+    }
+};
