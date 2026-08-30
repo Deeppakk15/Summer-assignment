910ab2ce9ea3727cf7a85416e3241178642755d4
Deepak gupta <sg7052551819@gmail.com>
2026-08-30 15:51:45 +0530

    Time: 112 ms (39.73%) | Memory: 170.6 MB (52.07%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,34 +1,65 @@
-class Solution {
-    void dfs(unordered_map<int, vector<int>> &adj,int u,int n,vector<bool>&visited,int &count){
-        visited[u]=true;
-        count++;
-        for(int &v:adj[u]){
-            if(!visited[v]){
-                  dfs(adj,v, n, visited,count);
-            }
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
     }
+    int Find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = Find(parent[x]);
+    }
+
+    void Union(int x, int y) {
+        int x_parent = Find(x);
+        int y_parent = Find(y);
+
+        if (x_parent == y_parent) {
+            return;
+        }
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if (rank[y_parent] > rank[x_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+};
+ class Solution {
 public:
     long long countPairs(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> adj;
-        vector<bool> visited(n, false);
-        for (auto edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        long long remainigsize=n;
-        int count=0;
-        long long ans=0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(adj,i, n, visited,count);
-              ans=ans+count*(remainigsize-count);    
-                remainigsize=remainigsize-count;
-                count=0;
-            }
-        }
-        return ans;
+   DSU dsu(n);
+
+for(auto edge:edges){
+    int u=edge[0];
+    int v=edge[1];
+
+    if(dsu.Find(u)!=dsu.Find(v)){
+        dsu.Union(u,v);
+    }
+}
+unordered_map<int,int>mp;
+for(int i=0;i<n;i++){
+  mp[dsu.Find(i)]++;
+}
+long long remainingsize=n;
+int req=n-1;
+long long ans=0;
+for(auto it :mp){
+ans=ans+it.second*(remainingsize-it.second);
+remainingsize=remainingsize-it.second;
+}
+
+return ans;
     }
 };
